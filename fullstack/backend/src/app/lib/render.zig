2c95c5445a670a4f3d40c570dec94ab06a1b3634
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-02-21 15:33:51 +0100

    changed 'fullstack/kickstart' 'fullstack/sveltekit' thanks to Allah

--- fullstack/backend/src/app/lib/render.zig
+++ fullstack/backend/src/app/lib/render.zig
@@ -6,5 +6,7 @@ const jetzig = @import("jetzig");
 pub fn render(request: *jetzig.Request, status: jetzig.http.status_codes.StatusCode, error_message: []const u8) !jetzig.View {
     var root = try request.data(.object);
     try root.put("error_message", error_message);
+    // lahdmdo li Allah this is a hack because we are using a seperate frontend
+    // try request.headers.append("accept", "application/json");
     return request.render(status);
 }
