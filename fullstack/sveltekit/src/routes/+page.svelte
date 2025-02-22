b0d6fc47c0eac99b9d6ac896b7153738ccb28de7
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-02-22 13:43:30 +0100

    installed skeletonui (new) thanks to Allah

--- fullstack/sveltekit/src/routes/+page.svelte
+++ fullstack/sveltekit/src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script>
+	// بسم الله الرحمن الرحيم
+	// la ilaha illa Allah Mohammed Rassoul Allah
+	import { TagsInput } from '@skeletonlabs/skeleton-svelte';
+
+	// State
+	let flavors = $state(['Vanilla', 'Chocolate', 'Strawberry']);
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<button class="btn  preset-filled">بسم الله الرحمن الرحيم</button>
+
+<TagsInput name="example" bind:value={flavors} placeholder="Add Tag..." />
--- fullstack/sveltekit/tailwind.config.js
+++ fullstack/sveltekit/tailwind.config.js
@@ -1,12 +1,26 @@
+// بسم الله الرحمن الرحيم
+// la ilaha illa Allah Mohammed Rassoul Allah
 import forms from '@tailwindcss/forms';
+
+import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
+import * as themes from '@skeletonlabs/skeleton/themes';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    contentPath(import.meta.url, 'svelte'),
+  ],
 
   theme: {
     extend: {}
   },
 
-  plugins: [forms]
+  plugins: [
+    forms,
+    skeleton({
+      // NOTE: each theme included will increase the size of your CSS bundle
+      themes: [ themes.cerberus, themes.rose ]
+    }),
+  ]
 };
