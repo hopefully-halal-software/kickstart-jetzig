17d0269d7b5753c4ea01d901d30b1e12b049d4ad
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-02-24 18:27:56 +0000

    added hero section thanks to Allah

--- fullstack/sveltekit/src/routes/+page.svelte
+++ fullstack/sveltekit/src/routes/+page.svelte
@@ -1,12 +1,24 @@
-<script>
-	// بسم الله الرحمن الرحيم
-	// la ilaha illa Allah Mohammed Rassoul Allah
-	import { TagsInput } from '@skeletonlabs/skeleton-svelte';
-
-	// State
-	let flavors = $state(['Vanilla', 'Chocolate', 'Strawberry']);
-</script>
-
-<button class="btn preset-filled">بسم الله الرحمن الرحيم</button>
-
-<TagsInput name="example" bind:value={flavors} placeholder="Add Tag..." />
+<div class="container mx-auto text-left p-4 md:text-center">
+	<div class="relative z-[1] space-y-6 pt-32 pb-24">
+		<div class="font-bold text-4xl tracking-tight">
+			لا إله إلا <span class="text-tertiary-500">الله</span> محمد رسول الله
+		</div>
+		<p class="max-w-lg opacity-70 md:mx-auto">
+			“la ilaha illa Allah” means nothing worshipped is worthy of worship except Allah. “Muhammadun Rasul Allah” is the assertion of the message of Prophet Muhammad and to believe in it and to adhere to it by speech, action and faith.
+		</p>
+		<div class="flex flex-wrap gap-3 md:space-x-3 md:block">
+			<a 
+				href="https://www.youtube.com/watch?v=1n-zYRZy5NQ"
+				class="bg-primary-300 btn md:w-fit variant-ringed-surface"
+			>
+				Oxford debate
+			</a>
+			<a 
+				href="https://youtube.com/playlist?list=PLPqH38Ki1fy3EB-8xmShVqpbQw99Do2B-&si=dZaEFV-pdpczi_fC"
+				class="md:btn-lg md:w-fit variant-ringed-surface"
+			>
+				Journey of Certainty →
+			</a>
+		</div>
+	</div>
+</div>
