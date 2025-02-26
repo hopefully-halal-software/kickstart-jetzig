1d1575ee25c15441f4b14b5bc0daa178086d11a9
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-02-26 15:10:30 +0000

    added logout thanks to Allah

---
+++ fullstack/jetzig/src/app/views/account/logout.zig
@@ -0,0 +1,49 @@
+// بسم الله الرحمن الرحيم
+// la ilaha illa Allah Mohammed rassoul Allah
+const std = @import("std");
+const jetzig = @import("jetzig");
+const tests = @import("../../lib/tests.zig");
+
+pub fn post(request: *jetzig.Request) !jetzig.View {
+    const session = try request.session();
+    try session.reset();
+    return request.redirect("/account/login", .found);
+}
+
+test "bismi_allah_post: no csrf" {
+    var app = try jetzig.testing.app(std.testing.allocator, @import("routes"));
+    defer app.deinit();
+
+    // need that for setting the csrf token thanks to Allah
+    _ = try app.request(.GET, "/account/login", .{});
+
+    try tests.login(&app);
+
+    const response = try app.request(.POST, "/account/logout", .{});
+
+    try std.testing.expect(!try app.session.remove("user"));
+
+    try response.expectStatus(.forbidden);
+}
+
+test "bismi_allah_post: with csrf" {
+    var app = try jetzig.testing.app(std.testing.allocator, @import("routes"));
+    defer app.deinit();
+
+    // need that for setting the csrf token thanks to Allah
+    _ = try app.request(.GET, "/account/login", .{});
+
+    try tests.login(&app);
+
+    const token = app.session.getT(.string, jetzig.authenticity_token_name).?;
+
+    const response = try app.request(
+        .POST,
+        "/account/logout",
+        .{ .params = .{ ._jetzig_authenticity_token = token } },
+    );
+
+    try std.testing.expect(!try app.session.remove("user"));
+
+    try response.expectStatus(.found);
+}
--- fullstack/sveltekit/src/routes/account/+page.svelte
+++ fullstack/sveltekit/src/routes/account/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   let { data } = $props();
   const user = data.user;
+  import AuthenticityFormElement from "$lib/components/authenticityFormElement.svelte";
 </script>
 
 <main>
@@ -8,5 +9,9 @@
   <p>Name: {user.name}</p>
   <p>Email: {user.email}</p>
   <p>Account Created At: {user.created_at}</p>
+  <form action="/account/logout" method="POST">
+	<AuthenticityFormElement {data}/>
+    <button type="submit" class="btn preset-filled-error-500">Logout</button>
+  </form>
 </main>
 
