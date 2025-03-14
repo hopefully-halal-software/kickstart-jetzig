d98fc9a798b0989f5573e00c32990e935132b6bb
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-03-14 23:52:54 +0000

    fixed lightSwitch problem all thanks to Allah

--- fullstack/sveltekit/src/lib/components/LightSwitch.svelte
+++ fullstack/sveltekit/src/lib/components/LightSwitch.svelte
@@ -1,36 +1,38 @@
 <script>
-    import { Switch } from '@skeletonlabs/skeleton-svelte';
-    // Icons
-    import IconMoon from 'lucide-svelte/icons/moon';
-    import IconSun from 'lucide-svelte/icons/sun';
+  import { Switch } from '@skeletonlabs/skeleton-svelte';
+  // Icons
+  import IconMoon from 'lucide-svelte/icons/moon';
+  import IconSun from 'lucide-svelte/icons/sun';
 
-    // Bind to the checked state
-    let mode = $state(false);
+  // Bind to the checked state
+  let mode = $state(false);
 
-    {
-	const darkMode = (() => {
-	    if (typeof localStorage !== 'undefined' && localStorage.getItem('darkMode')) return localStorage.getItem('darkMode');
-	    if (window.matchMedia('(prefers-color-scheme: dark)').matches) return 'dark';
-	    return 'dark';
-	})();
+  {
+    const darkMode = (() => {
+      if (typeof localStorage !== 'undefined' && localStorage.getItem('darkMode')) return localStorage.getItem('darkMode');
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches) return 'dark';
+        return 'dark';
+    })();
 
-	if (darkMode === 'light') {
-	    document.documentElement.classList.remove('dark');
-	} else {
-	    document.documentElement.classList.add('dark');
-	}
-
-	mode = 'dark' !== darkMode;
+    if (darkMode === 'light') {
+      document.documentElement.classList.remove('dark');
+    } else {
+      document.documentElement.classList.add('dark');
     }
 
-    // Handle the change in state when toggled.
-    function handleModeChange() {
-	const element = document.documentElement;
-	element.classList.toggle('dark');
+    mode = 'dark' !== darkMode;
+  }
 
-	const isDark = element.classList.contains('dark');
-	localStorage.setItem('darkMode', isDark ? 'dark' : 'light');
-    }
+  // Handle the change in state when toggled.
+  function handleModeChange() {
+    const element = document.documentElement;
+    element.classList.toggle('dark');
+
+    const isDark = element.classList.contains('dark');
+    localStorage.setItem('darkMode', isDark ? 'dark' : 'light');
+
+    mode = !mode;
+  }
 </script>
 
 <Switch name="mode" controlActive="bg-surface-200" bind:checked={mode} onCheckedChange={handleModeChange}>
