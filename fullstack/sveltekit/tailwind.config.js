de8b216d072a90163e8d8ef7cf6aeaa83620bdbf
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-03-14 15:00:11 +0000

    staging for tailwindcss4 migration all thanks to Allah

--- fullstack/sveltekit/tailwind.config.js
+++ fullstack/sveltekit/tailwind.config.js
@@ -2,26 +2,14 @@
 // la ilaha illa Allah Mohammed Rassoul Allah
 import forms from '@tailwindcss/forms';
 
-import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
-import * as themes from '@skeletonlabs/skeleton/themes';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'class',
-  content: [
-    './src/**/*.{html,js,svelte,ts}',
-    contentPath(import.meta.url, 'svelte'),
-  ],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
     extend: {}
   },
 
-  plugins: [
-    forms,
-    skeleton({
-      // NOTE: each theme included will increase the size of your CSS bundle
-      themes: [ themes.cerberus, themes.rose ]
-    }),
-  ]
+  plugins: [forms]
 };
